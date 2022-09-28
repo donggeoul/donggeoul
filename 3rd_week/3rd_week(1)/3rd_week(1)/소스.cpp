d1e8bc25157dcffeea3f1eb3655944cd10606466
donggeoul <113013154+donggeoul@users.noreply.github.com>
2022-09-28 20:11:27 +0900

    Add files via upload

---
+++ 3rd_week/3rd_week(1)/3rd_week(1)/소스.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int chest = 95;
+	char size;
+	if (chest <= 90)
+	{
+		size = 'S';
+	}
+	else if (chest <= 100)
+	{
+		size = 'M';
+	}
+	else
+	{
+		size = 'L';
+	}
+	return 0;
+}
